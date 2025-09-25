207e7ef179cd023061f7073b060ef17768b52a83
Ben Thake <0thakb77@solent.ac.uk>
2025-09-25 16:48:51 +0100

    Made flathead eat x2 and then display details, made cupra walk x4 and then display details

--- src/main.kt
+++ src/main.kt
@@ -8,4 +8,12 @@ fun main() {
 
     val flathead = Cat("Flathead", 7, 10)
     val cupra = Cat("Cupra", 2, 7)
+    flathead.eat()
+    flathead.eat()
+    println(flathead)
+    cupra.walk()
+    cupra.walk()
+    cupra.walk()
+    cupra.walk()
+    println(cupra)
 }
